d12b3af228bf5c7795d30788a4b57566abb9cbec
Derek Thomas <derek.thomas@dlaxon.com>
2021-05-20 18:02:49 -0600

    split Dropout from Conv2D into it's own module

--- tfne/encodings/codeepneat/modules/codeepneat_module_association.py
+++ tfne/encodings/codeepneat/modules/codeepneat_module_association.py
@@ -1,9 +1,9 @@
 from .codeepneat_module_densedropout import CoDeepNEATModuleDenseDropout
-from .codeepneat_module_conv2dmaxpool2ddropout import CoDeepNEATModuleConv2DMaxPool2DDropout
+from .codeepneat_module_conv2dmaxpool2d import CoDeepNEATModuleConv2DMaxPool2D
 
 # Dict associating the string name of the module when referenced in CoDeepNEAT config with the concrete instance of
 # the respective module
 MODULES = {
     'DenseDropout': CoDeepNEATModuleDenseDropout,
-    'Conv2DMaxPool2DDropout': CoDeepNEATModuleConv2DMaxPool2DDropout
+    'Conv2DMaxPool2D': CoDeepNEATModuleConv2DMaxPool2D
 }
